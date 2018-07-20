e483d7f6449c320bb961436a952a751aaed44e05
Jan Janeczko <janeczko.jan@hotmail.com>
2018-07-20 17:41:42 +0200

    update project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+);
+
 registerServiceWorker();
